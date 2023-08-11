813efdb38aeb3f8af523c45aa911249b85d877a4
Mateo <mateomautone98@gmail.com>
2023-08-11 13:51:12 -0300

    Cambios solicitados

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,18 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function funcionBoton() {
+    if (document.getElementById("password1").value === document.getElementById("password2").value &&
+        document.getElementById("password1").value.length >= 6 &&
+        document.getElementById("nombre").value !== "" &&
+        document.getElementById("apellido").value !== "" &&
+        document.getElementById("email").value !== "" &&
+        document.getElementById("terminos").checked) {
+        showAlertSuccess()
+    } else {
+        showAlertError()
+    }
+}
+
+document.getElementById("regBtn").onclick = function() {funcionBoton()}
